de87cb741448ce761e276f7b21f929fa352cac0c
Yeshtham Shah <121814183+Yeshthamshah@users.noreply.github.com>
2025-01-23 13:51:20 +0530

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,19 +8,7 @@ const AboutPage = () => {
         <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        Welcome to Trendify, your ultimate online shopping destination for all your needs! We offer a wide variety of carefully selected products across categories like fashion, gadgets, home goods, and more. Our mission is to make shopping easy, enjoyable, and accessible for everyone. With a user-friendly website, fast delivery, and top-notch customer service, we ensure that every order is smooth and hassle-free. Whether you’re searching for the latest trends or timeless classics, we’ve got something special just for you. Shop with us today and discover a better way to shop online!
         </p>
 
         <h2 className="text-center py-4">Our Products</h2>
